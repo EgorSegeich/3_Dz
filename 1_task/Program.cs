1fd8c0278280175730e1513d3fc52d6e3de50ee1
Egor <aldes@list.ru>
2022-12-11 01:10:49 +0300

    1 task

--- 1_task/Program.cs
+++ 1_task/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите 5-значное число  ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+int X1 = X / 10000;
+int X2 = (X - (X1*10000)) / 1000;
+int X3 = (X - (X1*10000) - (X2*1000)) / 100;
+int X4 = (X - (X1*10000) - (X2*1000) - (X3*100)) / 10;
+int X5 = (X - (X1*10000) - (X2*1000) - (X3*100)) % 10;
+
+int sum = X5*10000 + X4*1000 + X3*100 + X2*10 + X1%10;
+
+if (X == sum)
+{
+Console.WriteLine("да");
+}
+
+else
+{
+Console.WriteLine("нет");
+}
