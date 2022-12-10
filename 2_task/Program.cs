f19fe934669450f77f39a8cdecb7a8f7790797ae
Egor <aldes@list.ru>
2022-12-11 01:34:15 +0300

    2 task

---
+++ 2_task/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double dist = Math.Sqrt((x2-x1)+(y2-y1)+(z2-z1));
+Console.WriteLine(dist);
