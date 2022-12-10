79eaaf37f8ddd519ee25a1cc811ca5602ee03215
Egor <aldes@list.ru>
2022-12-11 00:02:59 +0300

    3 task

--- 3_task/Program.cs
+++ 3_task/Program.cs
@@ -1,32 +1,14 @@
-﻿Console.WriteLine("введите число  ");
+﻿void PrintArray (int[] a, int l)
+{
+    for (int i=0; i<l; i++)
+    {
+        a[i] = i + 1;
+        Console.WriteLine(a[i]*a[i]*a[i]);
+    }
+}
+
+Console.WriteLine("введите число  ");
 int N = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[N];
 
-for (int i=0; i<N; i++)
-{
-    array[i] = i + 1;
-    Console.WriteLine(array[i]*array[i]*array[i]);
-}
-
-
-/*
-//1
-for (int i=1; i<=N; i++)
-{
-    Console.WriteLine(i*i*i);
-}
-*/
-  
-
-
-/*
-//2
-int i = 0;
-int array[i] = 1;
-while (i < N)
-{
-    array[i] = array[i] + 1;
-    Console.WriteLine(sum);
-    i++;     
-}
-*/
+PrintArray(array, N);
